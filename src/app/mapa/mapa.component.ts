ba8f4bdbee1ad973d58bc410bef72b7edec1734f
Juliano Freitas <juliano-freitas@procergs.rs.gov.br>
2020-03-22 16:04:03 -0300

    Adicionado open street maps.

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+declare var ol: any;
 
 @Component({
-  selector: 'app-mapa',
-  templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.css']
+  selector: "app-mapa",
+  templateUrl: "./mapa.component.html",
+  styleUrls: ["./mapa.component.css"]
 })
 export class MapaComponent implements OnInit {
+  map: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.map = new ol.Map({
+      target: "map",
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new View({
+        center: ol.proj.fromLonLat([-30.115, -54.923]),
+        zoom: 6,
+        title: "Rio Grande do Sul"
+      })
+    });
   }
-
 }
