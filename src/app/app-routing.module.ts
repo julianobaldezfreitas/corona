50c46d232e869cafa2e02c2756162c32bac15a7b
Juliano Freitas <juliano-freitas@procergs.rs.gov.br>
2020-03-22 15:16:45 -0300

    Criação da estrutura basica projeto angular. Adicionado bootstrap.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CapaComponent } from "./capa/capa.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: CapaComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapaComponent } from './capa/capa.component';
+import { DadosComponent } from './dados/dados.component';
+import { MapaComponent } from './mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapaComponent,
+    DadosComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
