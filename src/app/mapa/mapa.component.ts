f14e1e57062d6e0a2abee69db98709d0d9ac6abb
Juliano Freitas <juliano-freitas@procergs.rs.gov.br>
2020-03-23 00:51:07 -0300

    Mapa do RS centralizado e início de código markers

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -13,18 +13,70 @@ export class MapaComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    let mousePositionControl = new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      projection: "EPSG:4326",
+      // comment the following two lines to have the mouse position
+      // be placed within the map.
+      className: "custom-mouse-position",
+      target: document.getElementById("mouse-position"),
+      undefinedHTML: "&nbsp;"
+    });
+
     this.map = new ol.Map({
       target: "map",
+      controls: ol.control
+        .defaults({
+          attributionOptions: {
+            collapsible: false
+          }
+        })
+        .extend([mousePositionControl]),
       layers: [
         new ol.layer.Tile({
           source: new ol.source.OSM()
         })
       ],
       view: new ol.View({
-        center: ol.proj.fromLonLat([-30.115, -54.923]),
-        zoom: 6,
-        title: "Rio Grande do Sul"
+        center: ol.proj.fromLonLat([-53.338623046875, -29.62122111378448]), //lat: -29.62122111378448 long: -53.338623046875
+        zoom: 6
       })
     });
+
+    this.addPoint(-29.62122111378448, -53.338623046875);
+
+    //this.map.on("click", function(args) {
+    //  console.log(args.coordinate);
+    //  var lonlat = ol.proj.transform(args.coordinate, "EPSG:3857", "EPSG:4326");
+    //  console.log(lonlat);
+
+    //  var lon = lonlat[0];
+    //  var lat = lonlat[1];
+    //  alert(`lat: ${lat} long: ${lon}`);
+    //});
+  }
+
+  addPoint(lat: number, lng: number) {
+    var vectorLayer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [
+          new ol.Feature({
+            geometry: new ol.geom.Point(
+              ol.proj.transform([lng, lat], "EPSG:4326", "EPSG:3857")
+            )
+          })
+        ]
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.5, 0.5],
+          anchorXUnits: "fraction",
+          anchorYUnits: "fraction",
+          src: "assets/img/my-icon.jpg"
+        })
+      })
+    });
+
+    // this.map.addLayer(vectorLayer);
   }
 }
