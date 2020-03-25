a66ab7993cda6cf4ef766f0bbe8059bb728bdf43
Juliano Freitas <juliano-freitas@procergs.rs.gov.br>
2020-03-24 23:08:55 -0300

    Adicoina markers hard coded dos municipios.

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -39,11 +39,42 @@ export class MapaComponent implements OnInit {
       ],
       view: new ol.View({
         center: ol.proj.fromLonLat([-53.338623046875, -29.62122111378448]), //lat: -29.62122111378448 long: -53.338623046875
-        zoom: 6
+        zoom: 7
       })
     });
 
-    this.addPoint(-29.62122111378448, -53.338623046875);
+    //this.addPoint(-29.62122111378448, -53.338623046875);
+    this.addPoint(-29.9914, -51.0809); //Alvorada
+    this.addPoint(-31.3297, -54.0999); //Bage
+    this.addPoint(-29.1662, -51.5165); //Bento Gonçalves
+    this.addPoint(-29.6747, -51.0606); //Campo bom
+    this.addPoint(-29.9128, -51.1857); //Canoas
+    this.addPoint(-29.7642, -50.0282); //Capão da Canoa
+    this.addPoint(-29.2969, -51.5028); // Carlos Barbosa
+    this.addPoint(-29.1629, -51.1792); //Caxias do Sul
+    this.addPoint(-29.9625, -51.6289); //Charqueadas
+    this.addPoint(-29.5148, -51.9928); //Cruzeiro do Sul
+    this.addPoint(-29.5836, -51.0898); //Doi Irmãos
+    this.addPoint(-30.0847, -51.6187); //Eldorado do Sul
+    this.addPoint(-27.6364, -52.2697); //Erechim
+    this.addPoint(-29.6535, -51.1843); //Estância Velha
+    this.addPoint(-29.5002, -51.9495); //Estrela
+    this.addPoint(-29.2227, -51.3419); //Farroupilha
+    this.addPoint(-29.5995, -51.1533); //Ivoti
+    this.addPoint(-29.4591, -51.9644); //Lajeado
+    this.addPoint(-29.8881, -50.2667); //Osório
+    this.addPoint(-30.0318, -51.2065); //Porto Alegre
+    this.addPoint(-32.0349, -52.1071); //Rio Grande
+    this.addPoint(-29.6868, -53.8149); //Santa Maria
+    this.addPoint(-30.8773, -55.5392); //Santana do Livramento
+    this.addPoint(-29.1897, -54.8666); //Santiago
+    this.addPoint(-29.8268, -50.5175); //Santo Antonio da Patrulha
+    this.addPoint(-29.7545, -51.1498); //São Leopoldo
+    this.addPoint(-29.6349, -51.0064); //Sapiranga
+    this.addPoint(-28.7126, -51.9352); //Serafina Correa
+    this.addPoint(-29.6505, -50.7753); //Taquara
+    this.addPoint(-29.3334, -49.7333); //Torres
+    this.addPoint(-30.0819, -51.0194); //Viamão
 
     //this.map.on("click", function(args) {
     //  console.log(args.coordinate);
